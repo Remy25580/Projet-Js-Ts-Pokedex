e60ef652eb8562a971c5b3ff8481cb487f606eb4
Remy25580 <remy.martinot@coda-student.school>
2026-01-15 14:56:08 +0100

    feat: creation de la page d'accueil PokPok

--- my-first-pokedex-coda/src/main.ts
+++ my-first-pokedex-coda/src/main.ts
@@ -1,24 +1,29 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from 'vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface PokeLink {
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function getPokemonIndic(id: number) {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}//`)
+  const result = await pokemon.json() as PokeLink;
+  return result;
+}
+
+const app = document.querySelector<HTMLDivElement>('#poke-liste')!
+
+for (let i = 1; i < 1026; i++){
+  const pok = await getPokemonIndic(i);
+  app.insertAdjacentHTML('beforeend',`
+    <div class="pokemon">
+      <p><img class="pokimage" src=${pok.sprites.front_default} alt="image de ${pok.name}"></p>
+      <p>${i}</p>
+      <p>${pok.name}</p>
+    </div>`)
+  }
+
+//document.querySelector<HTMLDivElement>('#app')!.innerHTML
