5c48dd62edc4c4af2495c02b0211fbf523644028
Remy25580 <remy.martinot@coda-student.school>
2026-01-22 14:36:54 +0100

    feat: ajout de css pour la barre de recherche sur index.html

--- my-first-pokedex-coda/src/search.ts
+++ my-first-pokedex-coda/src/search.ts
@@ -12,7 +12,7 @@ document.title = `PokSearch: ${search}`
 
 const app = document.querySelector<HTMLDivElement>('#recherche')!
 
-app.innerHTML = `You searched the ${type} ${search}`
+app.innerHTML = `<h3>You searched the ${type} ${search}</h3>`
 
 //Traitement des différents types de recherche
 switch(type){
@@ -120,4 +120,4 @@ switch(type){
         break;
 }
 
-app.insertAdjacentHTML('beforeend', `<h4>Fin de la recherche!</h4>`)
+app.insertAdjacentHTML('beforeend', `<h4>End of your research!</h4>`)
